25a3a888e366733bd7964d22b73cedb212d47c69
Joseph Bravo <josephangelobravo@gmail.com>
2022-08-01 16:17:36 -0700

    refactor to js

--- pages/api/[redirect].js
+++ pages/api/[redirect].js
@@ -1,10 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import redirects from '../../lib/redirects';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+/**
+ *
+ * @param {NextApiRequest} req
+ * @param {NextApiResponse} res
+ */
+export default function handler(req, res) {
   const { redirect } = req.query;
-  const path = redirects[redirect as any];
+  const path = redirects[redirect];
   if (!path) res.status(404).send('Not found');
   res.redirect(path);
 }
